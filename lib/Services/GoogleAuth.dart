89ab3a5968090d2aa2723a6ac1505e38379d66f2
anuteshome <ananyateshome2@gmail.com>
2026-03-06 02:31:31 +0300

    fix: make the google sign in to choose email account

--- lib/Services/GoogleAuth.dart
+++ lib/Services/GoogleAuth.dart
@@ -2,9 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Googleauth{
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
 
 Future <UserCredential?> signinGoogle ()async{
- final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+      await _googleSignIn.signOut();
+    await _googleSignIn.disconnect();
+
+ final GoogleSignInAccount? gUser = await _googleSignIn.signIn();
 if (gUser==null)return null;
 final GoogleSignInAuthentication gAuth= await gUser.authentication;
 
